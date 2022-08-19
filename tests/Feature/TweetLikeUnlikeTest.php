43acc328ccbd8a42ca91a7e962d5e16ccdc0aed2
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-19 18:15:02 +0300

    Added tweet liking tests

---
+++ tests/Feature/TweetLikeUnlikeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Tweet;
+use App\Models\User;
+
+test('a user can like a tweet',function (){
+    $tweet = Tweet::factory()->create();
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+        ->post(route('tweet.like',$tweet))
+        ->assertSessionHasNoErrors();
+
+    expect($tweet->likes)
+        ->toHaveCount(1)
+        ->first()->id->toBe($user->id)
+        ->first()->email->toBe($user->email)
+        ->first()->username->toBe($user->username)
+        ->first()->name->toBe($user->name);
+});
+
+test('a user can toggle a tweet like',function (){
+    $tweet = Tweet::factory()->create();
+    $user = User::factory()->create();
+    $tweet->likes()->attach($user->id);
+
+    $this->actingAs($user)
+        ->post(route('tweet.like',$tweet))
+        ->assertSessionHasNoErrors();
+
+    expect($tweet->likes)->toHaveCount(0);
+});
