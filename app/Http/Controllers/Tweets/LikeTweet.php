8aba66deffced4101d5f2466afe79eb9c71d4583
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-23 22:23:31 +0300

    phpstan

--- app/Http/Controllers/Tweets/LikeTweet.php
+++ app/Http/Controllers/Tweets/LikeTweet.php
@@ -8,14 +8,17 @@ use App\Events\TweetLikedEvent;
 use App\Models\Tweet;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeTweet
 {
     public function __invoke(Tweet $tweet, Request $request): RedirectResponse
     {
+        /** @var array<string,array<int,string>> $liked */
         $liked = $tweet->likes()->toggle($request->user())['attached'];
 
         if (collect($liked)->count()) {
+            /** @phpstan-ignore-next-line */
             TweetLikedEvent::dispatch($tweet, $request->user());
         }
 
