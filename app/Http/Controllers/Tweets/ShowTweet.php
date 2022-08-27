d2752d9100b6bb5f9021ff017b66b9636549672c
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-27 03:26:29 +0300

    Temporary redirect show tweet to home

--- app/Http/Controllers/Tweets/ShowTweet.php
+++ app/Http/Controllers/Tweets/ShowTweet.php
@@ -6,16 +6,18 @@ namespace App\Http\Controllers\Tweets;
 
 use App\Models\Tweet;
 use App\Models\User;
-use Inertia\Inertia;
-use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+//use Inertia\Inertia;
+//use Inertia\Response;
 
 class ShowTweet
 {
-    public function __invoke(User $user, Tweet $tweet): Response
+    public function __invoke(User $user, Tweet $tweet): RedirectResponse
     {
-        return Inertia::render(
-            component: 'Tweet',
-            props: ['tweet' => $tweet]
-        );
+        return redirect('/');
+//        return Inertia::render(
+//            component: 'Tweet',
+//            props: ['tweet' => $tweet]
+//        );
     }
 }
