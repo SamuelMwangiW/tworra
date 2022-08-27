082f9e5d416f4eed9311b0e43339776b9da0d720
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-27 03:11:21 +0300

    Fix stupid typos

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ composer install --no-dev --optimize-autoloader --prefer-dist --no-interaction
 php artisan migrate --force
 php artisan optimize
 php artisan view:clear
-php artisan events:clear
+php artisan event:clear
 php artisan view:cache
-php artisan events:cache
+php artisan event:cache
 
 sudo supervisorctl restart all
