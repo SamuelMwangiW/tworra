7aa7bf602b9d6610b48c6e3ba1b786607f318346
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-27 03:10:30 +0300

    Clear and cache events and views

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ composer install --no-dev --optimize-autoloader --prefer-dist --no-interaction
 
 php artisan migrate --force
 php artisan optimize
+php artisan view:clear
+php artisan events:clear
 php artisan view:cache
+php artisan events:cache
 
 sudo supervisorctl restart all
