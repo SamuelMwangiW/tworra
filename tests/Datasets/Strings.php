5433aad776cdf46ff61845fbdc98b4d17574c824
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-11-09 06:52:04 +0300

    Update Strings.php

--- tests/Datasets/Strings.php
+++ tests/Datasets/Strings.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 dataset('tweets', [
     fn () => fake()->sentences(asText: true),
     fn () => fake()->sentence(),
-    fn () => fake()->word(),
+    fn () => fake()->word() . ' tweet',
 ]);
