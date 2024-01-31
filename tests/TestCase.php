d1faa00bc75fcce6edea8a4df95e03ab57e203a8
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2024-01-31 23:51:20 +0300

    Ignote vite in phpunit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
