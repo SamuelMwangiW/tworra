02e74316d73bc857fbb55cfef02002f4d6a20bad
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2024-05-24 00:18:54 +0300

    laravel 11 (#425)
    * Update composer dependencies
    
    * Update npm dependencies
    
    * Drop support for php 8.1
    
    * Setup php before running pint
    
    * Adopt Laravel 11 slim skeleton structure
    
    * WIP
    
    * WIP

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+];
--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
@@ -39,7 +38,7 @@ test('email can be verified', function () {
     Event::assertDispatched(Verified::class);
 
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+    $response->assertRedirect('/?verified=1');
 })->skip(function () {
     return ! Features::enabled(Features::emailVerification());
 }, 'Email verification not enabled.');
